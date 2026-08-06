6b356bad05d584845789df77ec987503cddf9d35
Vishal Pant <vishal.pant1@gmail.com>
2026-08-06 12:05:15 +0530

    implementing entities

---
+++ src/main/java/com/shiorii/entities/Bookmark.java
@@ -0,0 +1,31 @@
+package com.shiorii.entities;
+
+public class Bookmark {
+    private long id;
+    private String title;
+    private String profileUrl;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getProfileUrl() {
+        return profileUrl;
+    }
+
+    public void setProfileUrl(String profileUrl) {
+        this.profileUrl = profileUrl;
+    }
+}

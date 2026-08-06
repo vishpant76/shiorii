334c94081d9b0278e39856fdf0410966128362f8
Vishal Pant <vishal.pant1@gmail.com>
2026-08-06 17:32:47 +0530

    implementing manager classes

---
+++ src/main/java/com/shiorii/managers/BookmarkManager.java
@@ -0,0 +1,56 @@
+package com.shiorii.managers;
+
+import com.shiorii.entities.Movie;
+import com.shiorii.entities.Book;
+import com.shiorii.entities.WebLink;
+
+public class BookmarkManager {
+    private static BookmarkManager instance = new BookmarkManager();
+    private BookmarkManager(){
+
+    }
+
+    public static BookmarkManager getInstance(){
+        return instance;
+    }
+
+    public Movie createMovie(long id, String title, String profileUrl, int releaseYear, String[] cast, String[] directors, String genre, double imdbRating){
+
+        Movie movie = new Movie();
+        movie.setId(id);
+        movie.setTitle(title);
+        movie.setProfileUrl(profileUrl);
+        movie.setReleaseYear(releaseYear);
+        movie.setCast(cast);
+        movie.setDirectors(directors);
+        movie.setGenre(genre);
+        movie.setImdbRating(imdbRating);
+
+        return movie;
+    }
+
+    public Book createBook(long id, String title, int publicationYear, String publisher, String[] authors, String genre, double amazonRating){
+
+        Book book = new Book();
+        book.setId(id);
+        book.setTitle(title);
+//        book.setProfileUrl(profileUrl);
+        book.setPublicationYear(publicationYear);
+        book.setPublisher(publisher);
+        book.setAuthors(authors);
+        book.setGenre(genre);
+        book.setAmazonRating(amazonRating);
+
+        return book;
+    }
+
+    public WebLink createWebLink(long id, String title, String url, String host){
+        WebLink webLink = new WebLink();
+        webLink.setId(id);
+        webLink.setTitle(title);
+        webLink.setUrl(url);
+        webLink.setHost(host);
+
+        return webLink;
+    }
+}
---
+++ src/main/java/com/shiorii/managers/UserManager.java
@@ -0,0 +1,26 @@
+package com.shiorii.managers;
+
+import com.shiorii.entities.User;
+
+public class UserManager {
+    private static UserManager instance = new UserManager();
+    private UserManager(){
+    }
+
+    public static UserManager getInstance(){
+        return instance;
+    }
+
+    public User createUser(long id, String email, String password, String firstName, String lastName, int gender, String userType){
+        User user = new User();
+        user.setId(id);
+        user.setEmail(email);
+        user.setPassword(password);
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setGender(gender);
+        user.setUserType(userType);
+
+        return user;
+    }
+}
